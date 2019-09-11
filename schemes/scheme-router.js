2ab06a5fb4be5de1d3ad7f333dd9a3e75449ba7b
VulpineReynard <milare8@gmail.com>
2019-09-11 18:05:45 -0400

    added the rest of hte helper functions and refactored route handlers to user them instead of db methods

--- schemes/scheme-router.js
+++ schemes/scheme-router.js
@@ -14,14 +14,26 @@ router.route("/")
       res.status(500).send({ message: "Something went wrong." })
     })
 })
+.post((req, res) => {
+  const schemeData = req.body;
 
-router.get('/:id', (req, res) => {
+  schemeHelper.add(schemeData)
+  .then(scheme => {
+    res.status(201).json(scheme);
+  })
+  .catch (err => {
+    res.status(500).json({ message: 'Failed to create new scheme' });
+  });
+});
+
+router.route("/:id")
+.get((req, res) => {
   const { id } = req.params;
 
-  Schemes.findById(id)
+  schemeHelper.findById(id)
   .then(scheme => {
     if (scheme) {
-      res.json(scheme);
+      res.status(200).json(scheme);
     } else {
       res.status(404).json({ message: 'Could not find scheme with given id.' })
     }
@@ -29,9 +41,44 @@ router.get('/:id', (req, res) => {
   .catch(err => {
     res.status(500).json({ message: 'Failed to get schemes' });
   });
+})
+.put((req, res) => {
+  const { id } = req.params;
+  const newScheme = req.body;
+
+  schemeHelper.findById(id)
+  .then(scheme => {
+    if (scheme) {
+      schemeHelper.update(newScheme, id)
+      .then(updatedScheme => {
+        res.json(updatedScheme);
+      });
+    } else {
+      res.status(404).json({ message: 'Could not find scheme with given id' });
+    }
+  })
+  .catch (err => {
+    res.status(500).json({ message: 'Failed to update scheme' });
+  });
+})
+.delete((req, res) => {
+  const { id } = req.params;
+
+  schemeHelper.remove(id)
+  .then(deleted => {
+    if (deleted) {
+      res.json({ removed: deleted });
+    } else {
+      res.status(404).json({ message: 'Could not find scheme with given id' });
+    }
+  })
+  .catch(err => {
+    res.status(500).json({ message: 'Failed to delete scheme' });
+  });
 });
 
-router.get('/:id/steps', (req, res) => {
+router.route("/:id/steps")
+.get((req, res) => {
   const { id } = req.params;
 
   Schemes.findSteps(id)
@@ -45,21 +92,8 @@ router.get('/:id/steps', (req, res) => {
   .catch(err => {
     res.status(500).json({ message: 'Failed to get steps' });
   });
-});
-
-router.post('/', (req, res) => {
-  const schemeData = req.body;
-
-  Schemes.add(schemeData)
-  .then(scheme => {
-    res.status(201).json(scheme);
-  })
-  .catch (err => {
-    res.status(500).json({ message: 'Failed to create new scheme' });
-  });
-});
-
-router.post('/:id/steps', (req, res) => {
+})
+.post((req, res) => {
   const stepData = req.body;
   const { id } = req.params; 
 
@@ -76,42 +110,6 @@ router.post('/:id/steps', (req, res) => {
   })
   .catch (err => {
     res.status(500).json({ message: 'Failed to create new step' });
-  });
-});
-
-router.put('/:id', (req, res) => {
-  const { id } = req.params;
-  const changes = req.body;
-
-  Schemes.findById(id)
-  .then(scheme => {
-    if (scheme) {
-      Schemes.update(changes, id)
-      .then(updatedScheme => {
-        res.json(updatedScheme);
-      });
-    } else {
-      res.status(404).json({ message: 'Could not find scheme with given id' });
-    }
-  })
-  .catch (err => {
-    res.status(500).json({ message: 'Failed to update scheme' });
-  });
-});
-
-router.delete('/:id', (req, res) => {
-  const { id } = req.params;
-
-  Schemes.remove(id)
-  .then(deleted => {
-    if (deleted) {
-      res.json({ removed: deleted });
-    } else {
-      res.status(404).json({ message: 'Could not find scheme with given id' });
-    }
-  })
-  .catch(err => {
-    res.status(500).json({ message: 'Failed to delete scheme' });
   });
 });
 
